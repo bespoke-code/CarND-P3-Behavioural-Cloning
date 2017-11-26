a9d2a4984efa902e9d226af22040cd42b8bc8955
Andrej Georgievski <andrej.zgeorgievski@gmail.com>
2017-11-26 05:55:10 +0100

    Fiddling with data preprocessing. Added image noise, did some testing and improvements

--- model-test.py
+++ model-test.py
@@ -5,7 +5,7 @@ import pandas as pd
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Convolution2D, MaxPooling2D, Cropping2D
 from keras.models import Sequential
-
+from os import getcwd
 
 def flip(image):
     return cv2.flip(image, 1)
@@ -33,12 +33,68 @@ def agument_dataset(images, measurements):
 
 
 if __name__ == '__main__':
-    image_name = "data/IMG/left_2016_12_01_13_34_23_036.jpg"
+    driving_data = csv_import("data/driving_log_test_1.csv")
+    dataset2 = csv_import("data/driving_log_test_2.csv")
+    driving_data = driving_data.append(dataset2, ignore_index=True)
+    #print(dataset1.iloc[3][:])
+    #print(dataset2.loc[3])
+    #print(driving_data.append(dataset2, ignore_index=True))
+    print(driving_data.shape)
+    images = []
+    for row in driving_data.itertuples():
+        # reading images in the following order:
+        # center, left, right
+        #print(row)
+        for column in np.arange(1, 4):
+            #print("Reading ", str(row[column]).strip(' '))
+            img = cv2.imread(str(row[column]).strip(' '))
+            #print(img.shape)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            images.append(img)
+    print("Loaded {num} images.".format(num=len(images)))
+    #plt.imshow(images[int(np.random.random_integers(0, len(images), size=1))])
+    #plt.show()
+
+    steering_angles = []
+    steering_bias = 0.25  # tune this
+    for row in driving_data.itertuples():
+        steering_angles.append(float(row[4]))
+        # add a corrective bias for each left image and -0.2 for each right camera image
+        steering_angles.append(float(row[4]) + steering_bias)
+        steering_angles.append(float(row[4]) - steering_bias)
+    print("Loaded {num} steering angles.".format(num=len(steering_angles)))
+
+    agu_imgs, agu_steer = agument_dataset(images, steering_angles)
+    #plt.imshow(agu_imgs[int(np.random.random_integers(0, len(agu_imgs), size=1))])
+    #plt.show()
+    #print(images)
+    print(len(images))
+    images.append(agu_imgs)
+    #print(images)
+    print(len(images))
+
+    #print(steering_angles)
+    #steering_angles.extend(agu_steer)
+    #print(steering_angles)
+
+    exit(0)
+    # Test print
+    #print(dataset.loc[3][6])
+
+    image_name = "data/2016/IMG/left_2016_12_01_13_34_23_036.jpg"
     image = cv2.imread(image_name)
-    cv2.imshow("Imported img", image)
+
+    noise = np.zeros_like(image)
+    noise = cv2.randn(noise, (0, 0, 0), (255, 255, 255))
+
+    cv2.imshow("Noise", noise)
+
+    image = cv2.addWeighted(image, 0.75, noise, 0.25, 0)
+    cv2.imshow("Noisy img", image)
+
     cv2.waitKey(0)
-    image = flip(image)
-    cv2.imshow("Flipped image", image)
-    cv2.waitKey(0)
+    #image = flip(image)
+    #cv2.imshow("Flipped image", image)
+    #cv2.waitKey(0)
     cv2.destroyAllWindows()
 
--- model.py
+++ model.py
@@ -2,13 +2,21 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers import Convolution2D, MaxPooling2D, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, GaussianNoise
+from keras.layers import Convolution2D, Cropping2D, Dropout
+from keras import regularizers
 from keras.models import Sequential
 
 
 def flip(image):
     return cv2.flip(image, 1)
+
+
+def add_noise(image):
+    noise = np.zeros_like(image)
+    noise = cv2.randn(noise, (0, 0, 0), (255, 255, 255))
+    noise = cv2.cvtColor(noise, cv2.COLOR_BGR2RGB)
+    return cv2.addWeighted(image, 0.75, noise, 0.25, 0)
 
 
 def csv_import(csv_filepath):
@@ -46,6 +54,9 @@ def create_keras_model():
     # udacity lambda normalization layer as seen in the lectures
     keras_model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=(160, 320, 3)))
 
+    # Add Gaussian noise to randomly agument the dataset.
+    #keras_model.add(GaussianNoise(2))
+
     # Add in the future and see how this should fit
     # model.add(Lambda(lambda x: cv2.cvtColor(x, cv2.COLOR_RGB2GRAY)))
 
@@ -53,12 +64,16 @@ def create_keras_model():
     keras_model.add(Convolution2D(24, (5, 5), strides=(2, 2), activation='relu'))
     keras_model.add(Convolution2D(36, (5, 5), strides=(2, 2), activation='relu'))
     keras_model.add(Convolution2D(48, (5, 5), strides=(2, 2), activation='relu'))
+    keras_model.add(Dropout(0.5))
     keras_model.add(Convolution2D(64, (3, 3), strides=(1, 1), activation='relu'))
     keras_model.add(Convolution2D(64, (3, 3), strides=(1, 1), activation='relu'))
+    keras_model.add(Dropout(0.5))
     keras_model.add(Flatten())
-    keras_model.add(Dense(100))
-    keras_model.add(Dense(50))
-    keras_model.add(Dense(10))
+    keras_model.add(Dense(100, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+    keras_model.add(Dropout(0.5))
+    keras_model.add(Dense(50, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+    keras_model.add(Dropout(0.5))
+    keras_model.add(Dense(10, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
     keras_model.add(Dense(1))
 
     return keras_model
@@ -71,7 +86,7 @@ if __name__ == '__main__':
     data_17 = csv_import("data/2017/" + log_name)
 
     # Center, Left, Right, Steering, Throttle, Brake, Speed
-    driving_data.append(data_17, ignore_index=True)
+    driving_data = driving_data.append(data_17, ignore_index=True)
 
     # organize images and measurements. fill arrays accordingly
     # as seen at: https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
@@ -79,28 +94,41 @@ if __name__ == '__main__':
     for row in driving_data.itertuples():
         # reading images in the following order:
         # center, left, right
-        for column in [0, 1, 2]:
-            img = cv2.imread(row[column])
-            # Maybe this will be useful:
-            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        for column in np.arange(1,4):
+            img = cv2.imread(row[column].strip(' '))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             images.append(img)
     print("Loaded {num} images.".format(num=len(images)))
 
     steering_angles = []
-    steering_bias = 0.2  # tune this
+    steering_bias = 0.25  # tune this
     for row in driving_data.itertuples():
-        steering_angles.append(float(row[3]))
-        # add a corrective bias for each right image and -0.2 for each left camera image
-        steering_angles.append(float(row[3]) + steering_bias)
-        steering_angles.append(float(row[3]) - steering_bias)
+        steering_angles.append(float(row[4]))
+        # add a corrective bias for each left image and -0.25 for each right camera image
+        steering_angles.append(float(row[4]) + steering_bias)
+        steering_angles.append(float(row[4]) - steering_bias)
     print("Loaded {num} steering angles.".format(num=len(steering_angles)))
 
+    # Flipping the images to extend the training set
+    agu_images, agu_steering_angles = agument_dataset(images, steering_angles)
+    images.extend(agu_images)
+    steering_angles.extend(agu_steering_angles)
+
+    for image in images:
+        image = add_noise(image)
+    print("Random noise added to each image.")
+
+
     # generate model and compile
+    print("Compiling the KERAS model!")
     model = create_keras_model()
     model.compile(optimizer='adam', loss='mse')
 
     # train the model
-    model.fit(x=images, y=steering_angles, validation_split=0.3, shuffle=True)
+    print("Training...")
+    model.fit(x=np.array(images), y=np.array(steering_angles), validation_split=0.3, shuffle=True)
 
     # save the model
+    print("Saving model...")
     model.save('model.h5')
+    print("Done!")
